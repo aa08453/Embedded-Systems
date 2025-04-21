4ba3923524f72aff29f0a0c55eab7dd2ae8684d0
Abdullah Amir <aa08453@st.habib.edu.pk>
2025-04-21 22:23:05 +0500

    some more logging

--- src/US.c
+++ src/US.c
@@ -3,29 +3,32 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/sys/printk.h>
 #include "../inc/US.h"
+#include <zephyr/logging/log.h>
 
+LOG_MODULE_REGISTER(us, CONFIG_LOG_DEFAULT_LEVEL);
 
 struct sensor_value distance;
 static const struct device *hcsr04_dev =  DEVICE_DT_GET_ANY(my_hc_sr04);
 
-void init_US(void) 
+int init_US(void) 
 {    
     if (!device_is_ready(hcsr04_dev)) 
     {
-        printk("HCSR04 sensor device not ready\n");
-        return;
+        LOG_ERR("HCSR04 sensor device not ready");
+        return -1;
     }
-    printk("HCSR04 sensor initialized\n");
+    LOG_INF("HCSR04 sensor initialized");
+    return 0;
 }
 
 int read_US(void) {
     if (sensor_sample_fetch(hcsr04_dev) < 0) {
-        printk("Failed to fetch sensor sample\n");
+        LOG_ERR("Failed to fetch sensor sample");
         return -1;
     }
 
     if (sensor_channel_get(hcsr04_dev, SENSOR_CHAN_DISTANCE, &distance) < 0) {
-        printk("Failed to get sensor channel data\n");
+        LOG_ERR("Failed to get sensor channel data");
         return -1;
     }
 
--- src/uart.c
+++ src/uart.c
@@ -2,6 +2,9 @@
 #include <zephyr/drivers/uart.h>
 #include <stdlib.h>
 #include "../inc/uart.h"
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(uart, CONFIG_LOG_DEFAULT_LEVEL);
 
 #define UART_DEVICE_NODE DT_CHOSEN(zephyr_console)
 static const struct device *uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
@@ -10,37 +13,36 @@ int get_user_frequency()
 {
     if (!device_is_ready(uart_dev)) 
     {
-        printk("UART device not found!\n");
+        LOG_ERR("UART device not found!\n");
         return 10;
     }
 
     char input[10] = {0};
     int index = 0, freq = 0;
 
-    // printk("\nEnter frequency (10Hz - 50Hz): ");
+    printk("\nEnter frequency (10Hz - 50Hz): ");
 
-    // while (index < (sizeof(input) - 1))
-    // {
-    //     uint8_t c;
-    //     while (uart_poll_in(uart_dev, &c) != 0) 
-    //         k_sleep(K_MSEC(10));  // Wait for input
+    while (index < (sizeof(input) - 1))
+    {
+        uint8_t c;
+        while (uart_poll_in(uart_dev, &c) != 0) 
+            k_sleep(K_MSEC(10));  // Wait for input
 
-    //     if (c == '\n' || c == '\r')
-    //         break;
+        if (c == '\n' || c == '\r')
+            break;
 
-    //     input[index++] = c;
-    //     printk("%c", c);
-    // }
+        input[index++] = c;
+        printk("%c", c);
+    }
 
-    // input[index] = '\0';
-    // freq = atoi(input); 
+    input[index] = '\0';
+    freq = atoi(input); 
 
-    // if (freq < 10 || freq > 50) 
-    // {
-    //     printk("\nInvalid frequency! Must be between 10Hz and 50Hz.\n");
-    //     return get_user_frequency(); 
-    // }
-    freq = 50;
+    if (freq < 10 || freq > 50) 
+    {
+        printk("\nInvalid frequency! Must be between 10Hz and 50Hz.\n");
+        return get_user_frequency(); 
+    }
     
     return freq;
 }
